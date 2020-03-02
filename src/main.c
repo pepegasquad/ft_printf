2f1b72da303906a8a2028efa175e3bd56b4a6a3a
arcrait <quadrio@bk.ru>
2020-03-02 12:46:49 +0300

    prot

--- src/main.c
+++ src/main.c
@@ -6,8 +6,15 @@
 /*   By: bkasandr <bkasandr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/02 09:33:54 by bkasandr          #+#    #+#             */
-/*   Updated: 2020/03/02 09:33:54 by bkasandr         ###   ########.fr       */
+/*   Updated: 2020/03/02 12:45:47 by bkasandr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int printf( const char *format , ...);
 
+int main(void)
+{
+	ft_printf("{%f}{%lf}\n", 1., 1.l);
+	printf("{%f}{%lf}", 1., 1.l);
+	return 0;
+}
